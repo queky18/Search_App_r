d3a4cfedb39df13cf518d904ae3852a8bc03730c
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-17 21:21:32 +0300

    req

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css';
 
 // State - the description of your app - an object that describe your application
 // State is able to change
@@ -26,7 +27,7 @@ class App extends Component {
         });
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className="f2">Search Friends</h1>
                 <SearchBox searchChange = { this.onSearchChange }/>
                 <CardList robots={filteredRobots}/>
             </div>
