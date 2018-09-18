0e737347cfeb61786205f3f9fc1c8e73d5cf91c4
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-18 22:02:37 +0300

    app few modificatiion

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
+
+import { setSearchField } from '../actions';
 
 // State - the description of your app - an object that describe your application
 // State is able to change
