8186b961122f75c2533ad79d2195f31f83036dbe
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-19 12:28:11 +0300

    Add executor

---
+++ src/components/Executors.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Executors = ({id, name, email}) => {
+
+    return (
+        <div className="robots tc bg-light-green dib br3 pa3 na2 grow bw2 shadow-5">
+            <img alt="Persona" src={`https://robohash.org/${id}?200x200`}/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Executors;
