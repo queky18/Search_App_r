1e2a5d669525f8d7dfc1ff9b32a7a53203d03509
Adrian Gheorghe <adrian.gh.m.18@gmail.com>
2018-09-20 13:09:59 +0300

    connect() redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Connect }{}
 import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
